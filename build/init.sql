cd34c1e6a40ba136da77b03fd7136f37919291fc
GavrisFlavius07 <student@itismeucci.com>
2026-04-20 09:20:02 +0200

    init.sql

--- build/init.sql
+++ build/init.sql
@@ -1,25 +1,20 @@
-CREATE TABLE `alunni` (
+CREATE TABLE `account` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(20) NOT NULL,
-  `cognome` VARCHAR(20) NOT NULL,
+  `tax_id` VARCHAR(16) NOT NULL UNIQUE,
+  `owner_name` VARCHAR(100) NOT NULL,
+  `balance` DECIMAL(17, 2) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `certificazioni` (
+CREATE TABLE `transaction` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `alunno_id` INT(11) NOT NULL,
-  `titolo` VARCHAR(100) NOT NULL,
-  `votazione` INT(3) NOT NULL CHECK (`votazione` BETWEEN 0 AND 100),
-  `ente` VARCHAR(100) NOT NULL,
+  `id_account` INT(11) NOT NULL,
+  `type` ENUM(`DEPOSIT`,`WITHDRAWL`) NOT NULL,
+  `amount` DECIMAL(17, 2) NOT NULL,
+  `description` VARCHAR(255),
+  `created_at` TIMESTAMP NOT NULL,
+  `balance_after` DECIMAL(17, 2),
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`alunno_id`) REFERENCES `alunni`(`id`) ON DELETE CASCADE
+  FOREIGN KEY (`id_account`) REFERENCES `account`(`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-INSERT INTO `alunni` (`nome`, `cognome`) VALUES
-('claudio', 'benve'),
-('ivan', 'bruno');
-
-INSERT INTO `certificazioni` (`alunno_id`, `titolo`, `votazione`, `ente`) VALUES
-(1, 'Certificazione Python', 85, 'Coursera'),
-(1, 'Certificazione SQL', 90, 'Udemy'),
-(2, 'Certificazione Java', 78, 'Oracle');
