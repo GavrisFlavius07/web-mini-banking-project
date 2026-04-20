6ac70c6ed46f54f934175e0449e00318becf7618
GavrisFlavius07 <student@itismeucci.com>
2026-04-20 09:50:16 +0200

    added readme and added currency in db schema

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
+# MINIBANKING API
+
+## Exposed api
+
+### Movimenti
+
+1. GET `/account/{id_account}/transaction` per ottenere l'elenco dei movimenti
+2. GET `/account/{id_account}/transaction/{id}` per ottenere il dettaglio di un movimento
+3. POST `/account/{id_account}/deposit` per registrare un deposito
+4. POST `/account/{id_account}/withdrawal` per registrare un prelievo
+5. PUT `/account/{id_account}/transaction/{id}` per modificare la descrizione di un movimento
+6. DELETE `/account/{id_account}/transaction/{id}` per eliminare un movimento secondo la regola scelta
+
+### Saldo
+
+7. GET `/account/{id_account}/balance` per ottenere il saldo attuale
+
+### Conversione del saldo
+
+8. GET `/account/{id_account}/balance/convert/fiat?to=USD` per convertire il saldo in una valuta fiat
+9. GET `/account/{id_account}/balance/convert/crypto?to=BTC` per convertire il saldo in una criptovaluta
+
 ## Su Linux
 `MY_UID=$(id -u) MY_GID=$(id -g) docker-compose up`
 
--- build/init.sql
+++ build/init.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `account` (
   `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `tax_id` VARCHAR(16) NOT NULL UNIQUE,
+  `tax_id` VARCHAR(16) UNIQUE NOT NULL,
   `owner_name` VARCHAR(100) NOT NULL,
   `balance` DECIMAL(17, 2) NOT NULL,
   `created_at` TIMESTAMP NOT NULL,
-  PRIMARY KEY (`id`)
+  `id_currency` INT(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`id_currency`) REFERENCES `currency`(`id`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE `transaction` (
@@ -17,4 +19,10 @@ CREATE TABLE `transaction` (
   `balance_after` DECIMAL(17, 2),
   PRIMARY KEY (`id`),
   FOREIGN KEY (`id_account`) REFERENCES `account`(`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `currency` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) UNIQUE NOT NULL,
+  PRIMARY KEY (`id`),
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
