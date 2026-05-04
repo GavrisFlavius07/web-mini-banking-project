620d77a150a23de1a8cb7b142670fa1cdd8b8e65
student <student@itismeucci.com>
2026-05-04 10:32:58 +0200

    insert e test dei dati nelle varie tabelle

--- php/index.php
+++ php/index.php
@@ -5,21 +5,12 @@ use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/controllers/AlunniController.php';
+require __DIR__ . '/controllers/AccountController.php';
 
 $app = AppFactory::create();
 
-$app->get('/test', function (Request $request, Response $response, array $args) {
-    $response->getBody()->write("Test page");
-    return $response;
-});
-
-$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
-    return $response;
-});
-
-$app->get('/alunni', "AlunniController:index");
+$app->get('/account',"AccountController:accounts");
+$app->get('/currency',"AccountController:currencies");
+$app->get('/trans',"AccountController:transactions");
 
 $app->run();
