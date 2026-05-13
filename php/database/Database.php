af1a887db360f3c30d3f752231b441e88541109f
gavrisflavius07 <gavris.flavius@itismeucci.com>
2026-05-13 11:41:46 +0200

    settato il file.env

--- php/database/Database.php
+++ php/database/Database.php
@@ -8,7 +8,7 @@ class Database extends mysqli
     {
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-        parent::__construct('my_mariadb', 'root', 'ciccio', 'banking');
+        parent::__construct('my_mariadb', 'root', '', 'banking');
         $this->set_charset('utf8mb4');
     }
 
